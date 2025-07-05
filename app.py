73616baf339938168b197c68ac637fe1c4d8bd6c
joaodupin <joaoantonio44@hotmail.com>
2025-07-05 13:21:08 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
+# Usuário fixo para login
+USUARIO = "adminjj"
+SENHA = "jard2149"
+
 @app.route("/")
-def home():
-    return "<h1>Bem-vindo à JJ Locadora de Veículos</h1>"
+def index():
+    return redirect("/login")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    erro = ""
+    if request.method == "POST":
+        user = request.form["usuario"]
+        senha = request.form["senha"]
+        if user == USUARIO and senha == SENHA:
+            return redirect("/painel")
+        else:
+            erro = "Usuário ou senha incorretos."
+    return render_template("login.html", erro=erro)
+
+@app.route("/painel")
+def painel():
+    return render_template("painel.html")
